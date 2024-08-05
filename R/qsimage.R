b86eb657e3e03967675a0bc62407c461bb9e0372
Malay Basu <malaykbasu@gmail.com>
2024-08-04 21:46:21 -0500

    Update qsimage.R

--- R/qsimage.R
+++ R/qsimage.R
@@ -1,16 +1,19 @@
 #' Saves an image of the global environment
 #' @export
-qsave_image <- function() {
+qsave_image <- function(file="~/session.qs", nthreads=8) {
   do.call(qs::qsavem,
           c(
             lapply(ls(envir = .GlobalEnv), as.symbol),
-            file = "~/session.qs",
-            nthreads = parallel::detectCores() / 2
+            #file = "~/session.qs",
+            file=file,
+            #nthreads = parallel::detectCores() / 2
+            nthreads = nthreads
           ))
 }
 
 #' Loads an image of the global environment
 #' @export
-qload_image <- function() {
-  qs::qreadm("~/session.qs", env = .GlobalEnv, nthreads = parallel::detectCores() / 2)
+qload_image <- function( file="~/session.qs", nthreads=8) {
+  #qs::qreadm("~/session.qs", env = .GlobalEnv, nthreads = parallel::detectCores() / 2)
+  qs::qreadm(file, env = .GlobalEnv, nthreads = nthreads)
 }
